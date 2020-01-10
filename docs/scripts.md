e99a0e6c98904309f767782d98b9384ed8a54f7d
reZach <reZach@users.noreply.github.com>
2020-01-10 14:40:54 -0600

    adding some doc files/comments

---
+++ docs/scripts.md
@@ -0,0 +1,2 @@
+# Scripts
+This page is specific to the scripts in the package.json file; what they do and why we have them.
--- webpack.development.js
+++ webpack.development.js
@@ -4,16 +4,16 @@ const path = require("path");
 
 module.exports = merge(base, {
   mode: "development",
-  devtool: "source-map",
+  devtool: "source-map", // Show the source map so we can debug when developing locally
   devServer: {
     host: "localhost",
     port: "40992",
-    hot: true,
-    compress: true,
-    contentBase: path.resolve(__dirname, "app/dist"),
-    watchContentBase: true,
+    hot: true, // Hot-reload this server if changes are detected
+    compress: true, // Compress (gzip) files that are served
+    contentBase: path.resolve(__dirname, "app/dist"), // Where we serve the local dev server's files from
+    watchContentBase: true, // Watch the content base for changes
     watchOptions: {
-      ignored: /node_modules/
+      ignored: /node_modules/ // Ignore this path, probably not needed since we define contentBase above
     }
   }
 })
