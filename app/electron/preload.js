02b249baa5ba76e805fbc3e558e716ed5064630b
reZach <reZach@users.noreply.github.com>
2020-01-19 22:23:58 -0600

    beginning to get store worked on!

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -3,11 +3,26 @@ const {
     ipcRenderer
 } = require("electron");
 const i18nextBackend = require("i18next-electron-fs-backend");
+const Store = require("secure-electron-store").default;
+
+let store;
+let storePath = "";
+try {
+    storePath = process.argv.filter(p => p.indexOf("storePath:") >= 0)[0].split(":")[1];    
+} catch (error) {
+    throw "Could not find 'additionalArguments' value beginning with 'storePath:' in your BrowserWindow. Please ensure this is set!";
+} finally {
+    store = new Store({
+        path: storePath
+    });
+}
+
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
     "api", {
-        i18nextElectronBackend: i18nextBackend.preloadBindings(ipcRenderer)
+        i18nextElectronBackend: i18nextBackend.preloadBindings(ipcRenderer),
+        store: store.preloadBindings(ipcRenderer)
     }
 );
