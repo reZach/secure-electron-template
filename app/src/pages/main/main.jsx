1c9cba1f1579584eda44384875b1b23a662ef0ef
reZach <reZach@users.noreply.github.com>
2020-01-20 10:43:10 -0600

    add logging to see why page was refreshing; use preventDefault from page refresh

--- app/src/pages/main/main.jsx
+++ app/src/pages/main/main.jsx
@@ -5,43 +5,63 @@ import { writeConfigRequest } from "secure-electron-store";
 import Detail from "Components/detail/detail";
 
 class Main extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            message: props.home.message
-        }
+    this.state = {
+      message: props.home.message
+    };
 
-        this.onChangeMessage = this.onChangeMessage.bind(this);
-        this.onSubmitMessage = this.onSubmitMessage.bind(this);
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onSubmitMessage = this.onSubmitMessage.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    Object.entries(this.props).forEach(
+      ([key, val]) =>
+        prevProps[key] !== val && console.log(`Prop '${key}' changed`)
+    );
+    if (this.state) {
+      Object.entries(this.state).forEach(
+        ([key, val]) =>
+          prevState[key] !== val && console.log(`State '${key}' changed`)
+      );
     }
+  }
 
-    onChangeMessage(event){
-        const { value } = event.target;
-        this.setState(state => ({
-            message: value
-        }));
-    }
+  onChangeMessage(event) {
+    const { value } = event.target;
+    this.setState(state => ({
+      message: value
+    }));
+  }
 
-    onSubmitMessage(){        
-        this.props.changeMessage(this.state.message);
-        window.api.store.send(writeConfigRequest, "motd", this.state.message);
-    }
+  onSubmitMessage(event) {
+    event.preventDefault();
+    this.props.changeMessage(this.state.message);
+    window.api.store.send(writeConfigRequest, "motd", this.state.message);
+  }
 
-    render() {
-        return <div>
-            Message of the day: {this.props.home.message}<br />
-            <form onSubmit={this.onSubmitMessage}>
-                <input value={this.state.message} onChange={this.onChangeMessage}></input>
-                <input type="submit"></input>
-            </form>
-            <Detail></Detail>
-        </div>;
-    }
+  render() {
+    return (
+      <div>
+        Message of the day: {this.props.home.message}
+        <br />
+        <form onSubmit={this.onSubmitMessage}>
+          <input
+            value={this.state.message}
+            onChange={this.onChangeMessage}
+          ></input>
+          <input type="submit"></input>
+        </form>
+        <Detail></Detail>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, props) => ({
-    home: state.home
+  home: state.home
 });
 const mapDispatch = { changeMessage };
 
