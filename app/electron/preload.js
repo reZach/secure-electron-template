c55c63d222e13414b7815d7bf28e60a9c7ac4a36
reZach <reZach@users.noreply.github.com>
2020-01-22 22:37:55 -0600

    electron store is functional (mvp); updated readme to reflect work updated translation to make template more professional redux store pulls from store on load

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -6,18 +6,8 @@ const fs = require("fs");
 const i18nextBackend = require("i18next-electron-fs-backend");
 const Store = require("secure-electron-store").default;
 
-let store;
-let storePath = "";
-try {
-    let arg = process.argv.filter(p => p.indexOf("storePath:") >= 0)[0];
-    storePath = arg.substr(arg.indexOf(":") + 1);    
-} catch (error) {
-    throw "Could not find 'additionalArguments' value beginning with 'storePath:' in your BrowserWindow. Please ensure this is set!";
-} finally {
-    store = new Store({
-        path: storePath
-    });
-}
+// Create the electron store to be made available in the renderer process
+let store = new Store();
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
