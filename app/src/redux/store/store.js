adb1472a396d0edf734c6d78f5e752a0aa649a2b
reZach <reZach@users.noreply.github.com>
2020-02-01 00:44:42 -0600

    starting to work on/test redux undo/redo

--- app/src/redux/store/store.js
+++ app/src/redux/store/store.js
@@ -7,7 +7,9 @@ export const history = createHashHistory();
 
 const store = configureStore({
   reducer: rootReducer(history),
-  middleware: [...getDefaultMiddleware(), routerMiddleware(history)]
+  middleware: [...getDefaultMiddleware({
+    serializableCheck: false
+  }), routerMiddleware(history)]
 });
 
 export default store;
