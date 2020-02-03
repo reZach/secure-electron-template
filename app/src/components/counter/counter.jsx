f44aa7f7c606e80724aa0cbbad66075feb2dc6c1
reZach <reZach@users.noreply.github.com>
2020-02-02 23:35:37 -0600

    testing undo/redo with our package

--- app/src/components/counter/counter.jsx
+++ app/src/components/counter/counter.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 import { connect } from "react-redux";
-import { increment, decrement } from "Redux/components/counter/counterSlice";
+import { UNDO, REDO } from "easy-redux-undo";
+import { increment, decrement, rando } from "Redux/components/counter/counterSlice";
 
 class Counter extends React.Component {
+
+  constructor(){
+    super();
+
+    this.inc = this.inc.bind(this);
+    this.dec = this.dec.bind(this);
+    this.undo = this.undo.bind(this);
+    this.redo = this.redo.bind(this);
+    this.rando = this.rando.bind(this);
+  }
+
+  inc(event){
+    this.props.increment();
+  }
+
+  dec(event) {
+    this.props.decrement();
+  }
+
+  undo(event){
+    this.props.UNDO();
+  }
+
+  redo(event){
+    this.props.REDO();
+  }
+
+  rando(event){
+    this.props.rando();
+  }
+
   render() {    
     return (
       <div>
+        <button onClick={this.rando}>rando</button>
+        <button onClick={this.undo}>undo</button>
+        <button onClick={this.dec}>dec</button>
         {this.props.counter.value}
+        <button onClick={this.inc}>inc</button>
+        <button onClick={this.redo}>redo</button>
       </div>      
     );
   }
@@ -15,6 +52,6 @@ class Counter extends React.Component {
 const mapStateToProps = (state, props) => ({
   counter: state.counter.present
 });
-const mapDispatch = { increment, decrement };
+const mapDispatch = { increment, decrement, rando, UNDO, REDO };
 
 export default connect(mapStateToProps, mapDispatch)(Counter);
--- app/src/redux/components/counter/counterSlice.js
+++ app/src/redux/components/counter/counterSlice.js
@@ -3,7 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const counterSlice = createSlice({
   name: "counter",
   initialState: {
-    value: 0
+    value: 0,
+    complex: [{
+      "a": 1,
+      "b": 2
+    }]
   },
   reducers: {
     increment(state, action) {
@@ -11,12 +15,27 @@ const counterSlice = createSlice({
     },
     decrement(state, action) {
       state.value--;
+    },
+    rando(state, action){
+      let rand = Math.floor(Math.random() * 3);
+      
+      if (rand === 0){
+        state.complex.push({
+          a: 4
+        });
+      } else if (rand === 1){
+        state.complex.push({
+          a: 23
+        });
+      } else if (rand === 2){
+        state.complex[0].a = 15;
+      }
     }
   }
 });
 
 // Export actions
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, rando } = counterSlice.actions;
 
 // Export reducer
 export default counterSlice.reducer;
