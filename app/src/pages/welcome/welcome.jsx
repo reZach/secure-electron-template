c9036c20fa2a0483f04d34f515c7a9483c448326
Zachary Wenta <reZach@users.noreply.github.com>
2020-02-09 14:00:16 -0600

    creating separate pages to call out to the different functionality this template has

--- app/src/pages/welcome/welcome.jsx
+++ app/src/pages/welcome/welcome.jsx
@@ -7,8 +7,18 @@ class Welcome extends React.Component {
   render() {
     return (
       <div id="welcome">
-        <h1 className="header">Thank you for trying out the secure-electron-template!</h1>
-        <Link to={ROUTES.MAIN}>Click me to go to the main page.</Link>
+        <h1 className="header">
+          Thank you for trying out the secure-electron-template!
+        </h1>
+        <div>
+          <Link to={ROUTES.MOTD}>View a sample of using the store.</Link><br />
+          <Link to={ROUTES.LOCALIZATION}>
+            View a sample of changing locales.
+          </Link> <br />
+          <Link to={ROUTES.UNDOREDO}>
+            View a sample of undo/redoing actions.
+          </Link>
+        </div>
       </div>
     );
   }
