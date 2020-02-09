df5098e3010a1a806eade09d122da7ffc42032de
reZach <reZach@users.noreply.github.com>
2020-02-09 14:00:16 -0600

    creating separate pages to call out to the different functionality this template has

--- app/src/pages/undo-redo/undo-redo.jsx
+++ app/src/pages/undo-redo/undo-redo.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
-import { UNDO, REDO } from "easy-redux-undo";
+import ROUTES from "Constants/routes";
+import { Link } from "react-router-dom";
+import { UNDO, REDO, GROUPBEGIN, GROUPEND } from "easy-redux-undo";
 import { increment, decrement } from "Redux/components/counter/counterSlice";
 import { add, remove } from "Redux/components/complex/complexSlice";
 
-class Undo extends React.Component {
+class UndoRedo extends React.Component {
   constructor() {
     super();
 
@@ -19,6 +21,10 @@ class Undo extends React.Component {
     // Undo-specific
     this.undo = this.undo.bind(this);
     this.redo = this.redo.bind(this);
+    this.undo2 = this.undo2.bind(this);
+    this.redo2 = this.redo2.bind(this);
+    this.groupbegin = this.groupbegin.bind(this);
+    this.groupend = this.groupend.bind(this);
   }
 
   inc(event) {
@@ -45,9 +51,28 @@ class Undo extends React.Component {
     this.props.REDO();
   }
 
+  undo2(event) {
+    this.props.UNDO(2);
+  }
+
+  redo2(event) {
+    this.props.REDO(2);
+  }
+
+  groupbegin(event) {
+    this.props.GROUPBEGIN();
+  }
+
+  groupend(event) {
+    this.props.GROUPEND();
+  }
+
   render() {
     return (
-      <div>
+      <div id="undoredo">
+        <Link to={ROUTES.WELCOME} className="left">
+          Go back
+        </Link>
         <h4>Undo/Redo</h4>
         <span>
           Try out modifying, and then undo/redoing the redux history below!
@@ -66,8 +91,12 @@ class Undo extends React.Component {
             <button onClick={this.remove}>remove</button>
           </div>
         </div>
+        <button onClick={this.undo2}>undo 2</button>
         <button onClick={this.undo}>undo</button>
         <button onClick={this.redo}>redo</button>
+        <button onClick={this.redo2}>redo 2</button>
+        <button onClick={this.groupbegin}>group begin</button>
+        <button onClick={this.groupend}>group end</button>
       </div>
     );
   }
@@ -77,6 +106,15 @@ const mapStateToProps = (state, props) => ({
   counter: state.undoable.present.counter,
   complex: state.undoable.present.complex
 });
-const mapDispatch = { increment, decrement, add, remove, UNDO, REDO };
+const mapDispatch = {
+  increment,
+  decrement,
+  add,
+  remove,
+  UNDO,
+  REDO,
+  GROUPBEGIN,
+  GROUPEND
+};
 
-export default connect(mapStateToProps, mapDispatch)(Undo);
+export default connect(mapStateToProps, mapDispatch)(UndoRedo);
