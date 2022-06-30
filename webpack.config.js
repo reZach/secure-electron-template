4d3e72f23c601bd866deb8f94d2e9c0aa3b4a58a
reZach <reZach@users.noreply.github.com>
2022-06-29 22:04:15 -0500

    add typescript support; couple of enhancements from sonarcube

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 module.exports = {
   target: "web", // Our app can run without electron
-  entry: ["./app/src/index.jsx"], // The entry point of our app; these entry points can be named and we can also have multiple if we'd like to split the webpack bundle into smaller files to improve script loading speed between multiple pages of our app
+  entry: ["./app/src/index.tsx"], // The entry point of our app; these entry points can be named and we can also have multiple if we'd like to split the webpack bundle into smaller files to improve script loading speed between multiple pages of our app
   output: {
     path: path.resolve(__dirname, "app/dist"), // Where all the output files get dropped after webpack is done with them
     filename: "bundle.js" // The name of the webpack bundle that's generated
@@ -38,13 +38,13 @@ module.exports = {
           }
         }
       },
-      // loads .js/jsx files
+      // loads .js/jsx/tsx files
       {
-        test: /\.jsx?$/,
+        test: /\.[jt]sx?$/,
         include: [path.resolve(__dirname, "app/src")],
         loader: "babel-loader",
         resolve: {
-          extensions: [".js", ".jsx", ".json"]
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
         }
       },
       // loads .css files
