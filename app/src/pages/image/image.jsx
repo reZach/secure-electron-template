e3e4c3706af4a5704ab77302cf2d4c631df4955b
reZach <reZach@users.noreply.github.com>
2022-07-19 00:38:40 -0500

    save SO url

--- app/src/pages/image/image.jsx
+++ app/src/pages/image/image.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import img from "Images/testimage.png";
+// https://stackoverflow.com/questions/59736269/electron-how-to-resolve-css-url-in-prod
 
 class Image extends React.Component {
   render() {
